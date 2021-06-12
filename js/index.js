d5589e742c71757ae4b23059b3f57529737e8eb1
megfon_control_panel <mkuysunov@gmail.com>
2021-06-12 22:27:26 +0500

    released basic project pattern

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
 const toogleBtn = document.querySelector('.toggle-btn');
 const header = document.querySelector('header');
+const header_menu_links = document.querySelector('.header-menu').querySelectorAll('a');
+const copyrightCurrentDate = document.querySelector('.copyright-current-date');
 
-toogleBtn.onclick = function () {
+function handleToggleBtn() {
   toogleBtn.classList.toggle('toggle-btn_active');
   header.classList.toggle('header_active');
-};
-
-function paralax(e) {
-  this.querySelectorAll('.header-elements').forEach((element) => {
-    const speed = element.getAttribute('data-speed');
-    const x = (window.innerWidth - e.pageX * speed) / 100;
-    element.style.transform = `translateX(${x}px)`;
-  });
 }
 
-document.addEventListener('mousemove', paralax);
+toogleBtn.onclick = handleToggleBtn;
+
+header_menu_links.forEach((link) => {
+  link.onclick = handleToggleBtn;
+});
+
+copyrightCurrentDate.innerHTML = new Date().getFullYear();
